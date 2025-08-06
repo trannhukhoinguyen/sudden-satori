2ba9910f34257dbc152ca0207d03ecd4f411ec6d
Patriarchal Zen <trannhukhoinguyen@gmail.com>
2025-08-06 17:35:37 +0700

    Update VacantHallCollection.ts

--- src/data/koans/VacantHallCollection.ts
+++ src/data/koans/VacantHallCollection.ts
@@ -5,7 +5,7 @@ export interface VacantHallCollection {
 }
 
 /*
-*
+* (虛堂集, Kidōshū): 6 quyển, có lời bình xướng của Lâm Tuyền Tùng Luân (林泉從倫), san hành vào năm đầu (1295) niên hiệu Nguyên Trinh (元貞) nhà Nguyên. Nguyên văn là Lâm Tuyền Lão Nhân Bình Xướng Đơn Hà Thuần Thiền Sư Tụng Cổ Hư Đường Tập (林泉老人評唱丹霞淳禪師頌古虛堂集, Rinsenrōjinhyōshōtankajunzenjijukokidōshū). Đây là tập thâu lục 100 tắc tụng cổ của Đơn Hà Tử Thuần (丹霞子淳) nhà Tống để làm kim chỉ nam cho hàng hậu học biện đạo. Từ các bài tụng cổ được thâu lục trong Đơn Hà Ngữ Lục (丹霞語錄), mỗi tắc có lời dạy chúng của Lâm Tuyền Lão Nhân, rồi được thêm vào trước ngữ cho cổ tắc do Đơn Hà nêu lên, kế đến thêm vào tụng cổ của Đơn Hà, cuối cùng có lời bình xướng của riêng tác giả.
 * */
 
 const VacantHallCollections: VacantHallCollection[] = [
