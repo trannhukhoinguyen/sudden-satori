cce9e65b9ce9b057a06a21167d8bdcff868907b0
trann <trannhukhoinguyen@gmail.com>
2025-08-06 17:21:31 +0700

    update

--- src/data/koans/VacantHallCollection.ts
+++ src/data/koans/VacantHallCollection.ts
@@ -5,12 +5,7 @@ export interface VacantHallCollection {
 }
 
 /*
-* (空谷集, Kūkokushū): 6 quyển,
-* san hành vào năm thứ 22 (1285) niên hiệu Chí Nguyên (至元),
-* nguyên văn là Lâm Tuyền Lão Nhân Bình Xướng Đầu Tử Thanh Hòa Thượng Tụng Cổ Không Cốc Tập (林泉老人評唱投子清和尚頌古空谷集, Rinsenrōjinhyōshōtōsuseioshōjukokūkokushū).
-* Đầu Tử Nghĩa Thanh (投子義清) thâu lục 100 tắc cơ duyên của cổ nhân, thêm vào lời tụng cổ cho mỗi tắc,
-* sau đó Đơn Hà Tử Thuần (丹霞子淳) còn thêm vào lời dạy chúng và trước ngữ,
-* kế đến Lâm Tuyền Tùng Luân (林泉從倫) ghi lời bình xướng
+*
 * */
 
 const VacantHallCollections: VacantHallCollection[] = [
