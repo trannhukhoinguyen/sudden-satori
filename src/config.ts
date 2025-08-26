bf9a5afed36aae5d2e20ff235e7fb7432bd8ed48
trann <trannhukhoinguyen@gmail.com>
2025-08-26 11:29:28 +0700

    update

--- src/config.ts
+++ src/config.ts
@@ -20,26 +20,26 @@ export const config: SiteConfig = {
       "Tham thiền không phải là ngồi thiền, ngồi thiền cũng không phải là tham thiền. " +
       "Nhiều người lầm tưởng rằng ngồi thiền tức tham thiền kỳ thực tham thiền không cần ngồi cũng được. / " +
       "Như Chư Tổ dạy: Phải khi lao động mà tập tham được mới tốt, nếu chỉ thích ngồi yên một chỗ vắng lặng mà tập tham thì khó hy vọng kiến tánh. \n" +
-      "Lục Tổ nói: \"Đạo do tâm ngộ, bất tại tọa\". " +
-      "Phẩm Tọa thiền trong Pháp Bảo Đàn Kinh nói đến Tọa thiền là tâm tọa (chứ không phải thân tọa) nghĩa là: ngoài đối với tất cả cảnh giới thiện ác tâm niệm chẳng khởi gọi là tọa, trong thấy tự tánh chẳng động gọi là thiền. " +
+      "Lục Tổ nói: \"Đạo do tâm ngộ, bất tại tọa\". / " +
+      "Phẩm Tọa thiền trong Pháp Bảo Đàn Kinh nói đến Tọa thiền là tâm tọa (chứ không phải thân tọa) nghĩa là: ngoài đối với tất cả cảnh giới thiện ác tâm niệm chẳng khởi gọi là tọa, trong thấy tự tánh chẳng động gọi là thiền. / " +
       "Vậy thế nào là tham thiền? Chữ tham tức là Nghi, Nghi tức là không hiểu, một việc gì đã hiểu rồi thì hết nghi, hết nghi tức là không có tham, cho nên tham thiền rất chú trọng cái nghi gọi là nghi tình. / " +
       "Muốn khởi lên cái nghi tình phải nhờ câu thoại đầu, gọi là tham thoại đầu. / " +
       "Thế nào là thoại đầu? " +
       "Theo Ngài Hư Vân giải thích rằng khi muốn nói một câu thoại thì phải khởi niệm rồi mới nói được, lúc chưa khởi niệm muốn nói gọi là thoại đầu, nếu đã khởi niệm muốn nói, mặc dù chưa nói ra cũng đã là thoại vĩ rồi. " +
-      "Hiện nay bắt đầu tham thiền thì chưa đến thoại đầu nhưng cũng không phải ở thoại vĩ, là từ thoại vĩ tiến đến thoại đầu, đang đi ở giữa đường một ngày kia sẽ tiến tới thoại đầu. " +
+      "Hiện nay bắt đầu tham thiền thì chưa đến thoại đầu nhưng cũng không phải ở thoại vĩ, là từ thoại vĩ tiến đến thoại đầu, đang đi ở giữa đường một ngày kia sẽ tiến tới thoại đầu. / " +
       "Lúc ấy câu thoại tự mất, nếu còn câu thoại thì chưa đến thoại đầu. " +
-      "Ngài Hư Vân có thí dụ rằng: \"Thoại đầu là cây gậy, nghi tình là đi, như người cụt chân muốn đi phải nhờ cây gậy, cũng như muốn khởi nghi tình phải nhờ câu thoại đầu vậy. \n" +
+      "Ngài Hư Vân có thí dụ rằng: \"Thoại đầu là cây gậy, nghi tình là đi, như người cụt chân muốn đi phải nhờ cây gậy, cũng như muốn khởi nghi tình phải nhờ câu thoại đầu vậy. / " +
       "Nói tham thoại đầu, thoại đầu thì nhiều lắm, muôn muôn ngàn ngàn kể không hết, bây giờ tôi chỉ đề ra 5 câu thoại đầu để cho người tham thiền tự mình lựa một câu, câu nào tự mình cảm thấy rất khó hiểu, hiểu không nổi thì câu đó thích hợp cho mình tham. " +
       "Chỉ được lựa một câu không cho lựa hai câu và sau khi quyết định câu nào rồi không cho đổi qua đổi lại, thẳng tới mà tham đến kiến tánh mới thôi. / " +
-      "5 câu thoại đầu là: \n" +
-      "Khi chưa có trời đất, ta là cái gì? \n" +
-      "Muôn pháp về một, một về chổ nào? \n" +
-      "Trước khi cha mẹ chưa sanh, mặt mũi bổn lai của ta ra sao? \n" +
-      "Sanh từ đâu đến, chết đi về đâu? \n" +
+      "5 câu thoại đầu là: / " +
+      "Khi chưa có trời đất, ta là cái gì? / " +
+      "Muôn pháp về một, một về chổ nào? / " +
+      "Trước khi cha mẹ chưa sanh, mặt mũi bổn lai của ta ra sao? / " +
+      "Sanh từ đâu đến, chết đi về đâu? / " +
       "Chẳng phải tâm, chẳng phải Phật, chẳng phải vật, là cái gì? / " +
       "Câu thoại đầu là câu hỏi, có hỏi thì phải có đáp, cũng như câu: \"Khi chưa có Trời đất, ta là cái gì?\" hỏi thầm trong bụng cảm thấy không hiểu thì đáp không ra, đáp không ra thì càng thấy thắc mắc, chính cái thắc mắc đó gọi là nghi tình. / " +
       "Hỏi câu thứ nhất đáp không ra thì tiếp tục hỏi câu thứ nhì, đáp không ra, tiếp tục hỏi câu thứ ba, cứ tiếp tục hỏi mãi. " +
-      "Ngày đêm không ngừng; bất cứ đi, đứng, nằm, ngồi, đang làm công việc tay chân hay bằng trí óc, đi bộ, đi xe, đang ăn cơm, đang đi cầu, đang ngủ mê, đều phải tiếp tục hỏi tới hoài, không giây phút gián đoạn. " +
+      "Ngày đêm không ngừng; bất cứ đi, đứng, nằm, ngồi, đang làm công việc tay chân hay bằng trí óc, đi bộ, đi xe, đang ăn cơm, đang đi cầu, đang ngủ mê, đều phải tiếp tục hỏi tới hoài, không giây phút gián đoạn. / " +
       "Người sơ tham thì hay quên cũng như một ngày 24 tiếng đồng hồ, gián đoạn 23 tiếng, tập tham dần dần thì sự gián đoạn giảm bớt còn 22 tiếng, rồi tiếp tục còn 21 tiếng, 20 tiếng... dần dần đến công phu miên mật tức là ngày đêm 24 giờ không giây phút gián đoạn. / " +
       "Khi công phu được thành khối cũng gọi là đến thoại đầu, cũng gọi là đến đầu sào trăm thước. " +
       "Từ đầu sào trăm thước tiến thêm một bước nữa là kiến tánh, đạt đến chỗ tự do tự tại, được giải thoát vĩnh viễn tất cả khổ. ",
