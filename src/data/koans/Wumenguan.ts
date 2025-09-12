0ce66c096f6f8edc06f4298c382eb34e9d667f40
trann <trannhukhoinguyen@gmail.com>
2025-09-12 08:59:01 +0700

    update content

--- src/data/koans/Wumenguan.ts
+++ src/data/koans/Wumenguan.ts
@@ -4,8 +4,55 @@ import type { KoanTitle, Koan } from "@/types.ts";
 * * */
 
 export const WumenguanTitles: KoanTitle[] = [
-
-]
+  { title: "CON CHÓ CỦA TRIỆU CHÂU", path: "/koans/wmg01-cun-con-co-tanh-khong" },
+  { title: "CON CHỒN HOANG CỦA BÁCH TRƯỢNG", path: "/koans/wmg02-bat-muoi-nhan-qua" },
+  { title: "NGÓN TAY CỦA CÂU CHI", path: "/koans/wmg003-ngon-tay" },
+  { title: "RỢ HỒ KHÔNG RÂU", path: "/koans/wmg04-khong-rau" },
+  { title: "HƯƠNG NGHIÊM TRÊN CÂY", path: "/koans/wmg05-treo-tren-canh-cay" },
+  { title: "THẾ TÔN GIƠ HOA", path: "/koans/" },
+  { title: "TRIỆU CHÂU RỬA BÁT", path: "/koans/" },
+  { title: "HỀ TRỌNG LÀM XE", path: "/koans/" },
+  { title: "PHẬT ĐẠI THÔNG TRÍ THẮNG", path: "/koans/" },
+  { title: "THANH THOÁT LẺ LOI VÀ NGHÈO KHÓ", path: "/koans/" },
+  { title: "TRIỆU CHÂU KHÁM PHÁ AM CHỦ", path: "/koans/" },
+  { title: "NHAM GỌI CHỦ NHÂN", path: "/koans/" },
+  { title: "ĐỨC SƠN BƯNG BÁT", path: "/koans/" },
+  { title: "NAM TUYỀN CHÉM MÈO", path: "/koans/" },
+  { title: "BA GẬY CỦA ĐỘNG SƠN", path: "/koans/" },
+  { title: "NGHE TIẾNG CHUÔNG MẶC ÁO BẨY MẢNH", path: "/koans/" },
+  { title: "BA LẦN GỌI CỦA QUỐC SƯ", path: "/koans/" },
+  { title: "BA CÂN GAI CỦA ĐỘNG SƠN", path: "/koans/" },
+  { title: "TÂM BÌNH THƯỜNG LÀ ĐẠO", path: "/koans/" },
+  { title: "ĐẠI LỰC SĨ", path: "/koans/" },
+  { title: "QUE CỨT KHÔ CỦA VÂN MÔN", path: "/koans/" },
+  { title: "CÂY PHƯỚN CỦA CA DIẾP", path: "/koans/" },
+  { title: "CHẲNG PHẢI THIỆN ÁC", path: "/koans/" },
+  { title: "LÌA KHỎI NGÔN NGỮ", path: "/koans/" },
+  { title: "HÀNG THỨ BA NÓI PHÁP", path: "/koans/" },
+  { title: "HAI TĂNG CUỐN RÈM", path: "/koans/" },
+  { title: "KHÔNG PHẢI TÂM, PHẬT", path: "/koans/" },
+  { title: "NGHE TIẾNG LONG ĐÀM ĐÃLÂU", path: "/koans/" },
+  { title: "KHÔNG PHẢI GIÓ, KHÔNG PHẢI CỜ", path: "/koans/" },
+  { title: "TÂM LÀ PHẬT", path: "/koans/" },
+  { title: "TRIỆU CHÂU KHÁM PHÁ BÀ LÃO", path: "/koans/" },
+  { title: "NGOẠI ĐẠO HỎI PHẬT", path: "/koans/" },
+  { title: "CHẲNG TÂM, CHẲNG PHẬT", path: "/koans/" },
+  { title: "TRÍ CHẲNG LÀ ĐẠO", path: "/koans/" },
+  { title: "THIẾN NỮ LÌA HỒN", path: "/koans/" },
+  { title: "TRÊN ĐƯỜNG GẬP NGƯỜI ĐẠT ĐẠO", path: "/koans/" },
+  { title: "CÂY BÁCH TRƯỚC SÂN", path: "/koans/" },
+  { title: "CON TRÂU QUA CỬA", path: "/koans/" },
+  { title: "CÂU NÓI SAI CỦA VÂN MÔN", path: "/koans/" },
+  { title: "ĐÁ ĐỔ TỊNH BÌNH", path: "/koans/" },
+  { title: "ĐẠT MA AN TÂM", path: "/koans/" },
+  { title: "THIẾU NỮ XUẤT ĐỊNH", path: "/koans/" },
+  { title: "TRÚC BỀ CỦA THỦ SƠN", path: "/koans/" },
+  { title: "CÂY GẬY CỦA BA TIÊU", path: "/koans/" },
+  { title: "HẮN LÀ AI ?", path: "/koans/" },
+  { title: "ĐẦU GẬY BƯỚC THÊM", path: "/koans/" },
+  { title: "ĐÂU XUẤT BA CỬA", path: "/koans/" },
+  { title: "MỘT ĐƯỜNG CỦA CÀN PHONG", path: "/koans/" },
+];
 
 const Wumenguan: Koan[] = [
   {
@@ -18,55 +65,52 @@ const Wumenguan: Koan[] = [
 export default Wumenguan;
 
 /*
-*01 CON CHÓ CỦA TRIỆU CHÂU
-02 CON CHỒN HOANG CỦA BÁCHTRƯỢNG.
-03 NGÓN TAY CỦA CÂU CHI
-04 RỢ HỒ KHÔNG RÂU
-05 HƯƠNG NGHIÊM TRÊN CÂY
-06 THẾ TÔN GIƠ HOA
-07 TRIỆU CHÂU RỬA BÁT
-08 HỀ TRỌNG LÀM XE
-09 PHẬT ĐẠI THÔNG TRÍ THẮNG
-10 THANH THOÁT LẺ LOI VÀ NGHÈO KHÓ
-11 TRIỆU CHÂU KHÁM PHÁ AM CHỦ
-TỪ TẮC 12 ĐẾN TẮC 24
-12 NHAM GỌI CHỦ NHÂN
-13 ĐỨC SƠN BƯNG BÁT
-14 NAM TUYỀN CHÉM MÈO
-15 BA GẬY CỦA ĐỘNG SƠN
-16 NGHE TIẾNG CHUÔNG MẶC ÁO BẨY MẢNH
-17 BA LẦN GỌI CỦA QUỐC SƯ
-18 BA CÂN GAI CỦA ĐỘNG SƠN
-19 TÂM BÌNH THƯỜNG LÀ ĐẠO
-20 ĐẠI LỰC SĨ
-21 QUE CỨT KHÔ CỦA VÂN MÔN
-22 CÂY PHƯỚN CỦA CA DIẾP
-23 CHẲNG PHẢI THIỆN ÁC
-24 LÌA KHỎI NGÔN NGỮ
-TỪ TẮC 25 ĐẾN TẮC 36
-25 HÀNG THỨ BA NÓI PHÁP
-26 HAI TĂNG CUỐN RÈM
-27 KHÔNG PHẢI TÂM, PHẬT
-28 NGHE TIẾNG LONG ĐÀM ĐÃLÂU
-29 KHÔNG PHẢI GIÓ, KHÔNG PHẢI CỜ
-30 TÂM LÀ PHẬT
-31 TRIỆU CHÂU KHÁM PHÁ BÀ LÃO
-32 NGOẠI ĐẠO HỎI PHẬT
-33 CHẲNG TÂM, CHẲNG PHẬT
-34 TRÍ CHẲNG LÀ ĐẠO
-35 THIẾN NỮ LÌA HỒN
-36 TRÊN ĐƯỜNG GẬP NGƯỜI ĐẠT ĐẠO
-TỪ TẮC 37 ĐẾN TẮC 48
-37 CÂY BÁCH TRƯỚC SÂN
-38 CON TRÂU QUA CỬA
-39 CÂU NÓI SAI CỦA VÂN MÔN
-40 ĐÁ ĐỔ TỊNH BÌNH
-41 ĐẠT MA AN TÂM
-42 THIẾU NỮ XUẤT ĐỊNH
-43 TRÚC BỀ CỦA THỦ SƠN
-44 CÂY GẬY CỦA BA TIÊU
-45 HẮN LÀ AI ?
-46 ĐẦU GẬY BƯỚC THÊM
-47 ĐÂU XUẤT BA CỬA
-48 MỘT ĐƯỜNG CỦA CÀN PHONG
+CON CHÓ CỦA TRIỆU CHÂU
+CON CHỒN HOANG CỦA BÁCH TRƯỢNG
+NGÓN TAY CỦA CÂU CHI
+RỢ HỒ KHÔNG RÂU
+HƯƠNG NGHIÊM TRÊN CÂY
+THẾ TÔN GIƠ HOA
+TRIỆU CHÂU RỬA BÁT
+HỀ TRỌNG LÀM XE
+PHẬT ĐẠI THÔNG TRÍ THẮNG
+THANH THOÁT LẺ LOI VÀ NGHÈO KHÓ
+TRIỆU CHÂU KHÁM PHÁ AM CHỦ
+NHAM GỌI CHỦ NHÂN
+ĐỨC SƠN BƯNG BÁT
+NAM TUYỀN CHÉM MÈO
+BA GẬY CỦA ĐỘNG SƠN
+NGHE TIẾNG CHUÔNG MẶC ÁO BẨY MẢNH
+BA LẦN GỌI CỦA QUỐC SƯ
+BA CÂN GAI CỦA ĐỘNG SƠN
+TÂM BÌNH THƯỜNG LÀ ĐẠO
+ĐẠI LỰC SĨ
+QUE CỨT KHÔ CỦA VÂN MÔN
+CÂY PHƯỚN CỦA CA DIẾP
+CHẲNG PHẢI THIỆN ÁC
+LÌA KHỎI NGÔN NGỮ
+HÀNG THỨ BA NÓI PHÁP
+HAI TĂNG CUỐN RÈM
+KHÔNG PHẢI TÂM, PHẬT
+NGHE TIẾNG LONG ĐÀM ĐÃLÂU
+KHÔNG PHẢI GIÓ, KHÔNG PHẢI CỜ
+TÂM LÀ PHẬT
+TRIỆU CHÂU KHÁM PHÁ BÀ LÃO
+NGOẠI ĐẠO HỎI PHẬT
+CHẲNG TÂM, CHẲNG PHẬT
+TRÍ CHẲNG LÀ ĐẠO
+THIẾN NỮ LÌA HỒN
+TRÊN ĐƯỜNG GẬP NGƯỜI ĐẠT ĐẠO
+CÂY BÁCH TRƯỚC SÂN
+CON TRÂU QUA CỬA
+CÂU NÓI SAI CỦA VÂN MÔN
+ĐÁ ĐỔ TỊNH BÌNH
+ĐẠT MA AN TÂM
+THIẾU NỮ XUẤT ĐỊNH
+TRÚC BỀ CỦA THỦ SƠN
+CÂY GẬY CỦA BA TIÊU
+HẮN LÀ AI ?
+ĐẦU GẬY BƯỚC THÊM
+ĐÂU XUẤT BA CỬA
+MỘT ĐƯỜNG CỦA CÀN PHONG
 * */
--- src/types.ts
+++ src/types.ts
@@ -19,6 +19,7 @@ export interface Master {
 
 export interface KoanTitle {
     title: string;
+    path: string;
 }
 
 export interface Koan {
