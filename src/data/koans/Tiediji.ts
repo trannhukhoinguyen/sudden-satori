83d78b014784a953a30f974b9adcd3b8fcc10cdb
Patriarchal Zen <trannhukhoinguyen@gmail.com>
2025-09-12 12:58:39 +0700

    Update Tiediji.ts

--- src/data/koans/Tiediji.ts
+++ src/data/koans/Tiediji.ts
@@ -7,7 +7,108 @@ export const TiedijiTitles: KoanTitle[] = [
 /*
 *
 * 🇻🇳 Tiếng Sáo Thép / Thiết Địch Đảo Xuy / Thiết Sáo Tập – 🇺🇸 Iron Flute (🇨🇳 鐵笛集 Tiědíjí, 🇯🇵 Tetteki Tōsui)
-*
+*Bảng Liệt Kê Các Công Án 
+ 
+1. Văn Thù Vào Cửa, 15 
+2. La Sơn Khai Đường, 16 
+3. Nam Tuyền Phật Đá, 18 
+4. Bách Linh Chứng Đắc, 19 
+5. Thiều Sơn Một Câu. 21 
+6. Đầu Tử Thọ Trai, 22 
+7. Vân Môn Tiệc ở Miếu Thần, 23 
+8. Vân Cư Chỉ Dạy, 24 
+9. Từ Minh Đại Ý, 26 
+10. Dược Sơn Nắm Giữ, 28 
+11. Triệu Châu Trùm Đầu , 29 
+12. Tam Thánh Gặp Học Nhân, 31 
+13. Tiệm Nguyên Rèm Giấy, 33 
+14. Bạch Vân Trắng và Đen, 35
+15. Đại Tùy Tu Bên Trong, 37
+16. Qui Sơn Thời Gian, 38
+17. Đại Tùy Con Rùa, 40
+18. Lâm Tế Trồng Tùng, 41 
+19. Triệu Châu Viếng Chùa, 41 
+20. Đức Sơn Hiền Đức Ngày Xưa, 43 
+21. Phần Dương Cây Gậy, 44 
+22. Ba Lăng Mật Truyền, 45 
+23. Tuyết Phong Chặt Cây, 4 
+24. Nam Tuyền Tuổi Phật, 47 
+25. Nham Đầu Thùng Nước, 42 
+26. Tuyết Phong Chính Xác, 49 
+27. Ngưỡng Sơn Ngàn Muôn Cảnh, 50 
+28. Long Nha Cảnh Giới Vô Thượng, 51 
+29. Ngưỡng Sơn Chào Thầy, 53 
+30. Thái Tông Nằm Mộng, 54 
+31. Qui Sơn Gọi Hai Tăng Chức, 55 
+32. Phần Dương Trừng Phạt Hư Không, 56 
+33. Dược Sơn Vấn Đề, 57 
+34. Tuyết Phong Thấy Phật Tánh, 58 
+35. Lợi Tung Bài Thơ, 59 
+36. Sau Khi Chết Gặp Nhau Ở Đâu, 61 
+37. Tuyết Phong Thánh Tính, 62 
+38. Ra Đi và Trở Lại, 63 
+39. Ba Lần Gọi, 65 
+40. Khe Suối Khô, 66 
+41. Động Sơn Ba Tạng, 67 
+42. Núi Nam, 68 
+43. Đạo Lý Tối Hậu của Thiền, 69 
+44. Nam Tuyền Chẳng Nhận Cả Tăng Lẫn Tục, 70 
+45. Vu Địch Hỏi Phật, 72 
+46. Chữ Tâm, 73 
+47. Triệu Châu Đo Nước, 74 
+48. Địa Tạng Phật Pháp, 75 
+49. Huyền Sa Giấy Trắng, 77 
+50. Nghĩa Trung Thuyết Pháp, 78
+51. Bảo Phúc Cái Chùa, 79 
+52. Hoa Nghiêm Trở Lại Thế Gian Mê Hoặc, 81 
+53. Huệ Trung Đánh Đuổi Đệ Tử, 82 
+54. Nham Đầu Hai Bữa Ăn, 83 
+55. Mục Châu Kẻ Độn Căn, 85 
+56. Lỗ Tổ Quay Mặt Vào Tường, 86 
+57. Lâm Tế Người Không Ngôi Vị, 87 
+58. Pho Tượng Quan Thế Âm, 89 
+59. Vô Nghiệp Vọng Tưởng, 90 
+60. Cái Gối Gỗ, 92 
+61. Vân Môn Quả Thánh, 93 
+62. Nam Tuyền Am Nhỏ, 95 
+63. Dược Sơn Thuyết Pháp, 96 
+64. Cảnh Thanh Gậy To, 97 
+65. Vật Kỳ Diệu Nhất, 99 
+66. Đạo Ngô Đạo Lý Thâm Sâu, 99 
+67. Càn Phong Luân Hồi, 101 
+68. Vân Môn Ba Ngày, 103 
+69. Quan Thượng Thư, 104 
+70. Triệu Châu Trụ Chỗ Nào, 106 
+71. Vân Môn Nếp Nhà, 108 
+72. Bảo Thọ Quay Lưng, 110 
+73. Tuyết Phong Từ Chối Ông Tăng, 111 
+74. Chọn Người Khai Sáng Một Ngôi Chùa, 114 
+75. Ông Tăng Ngồi Thiền, 117 
+76. Địa Tạng Hoa Mẫu Đơn, 119 
+77. Động Sơn Khuyên Tăng, 120 
+78. Vân Cư Tặng Y, 122 
+79. Đức Sơn Giáo Pháp Tột Cùng, 124 
+80. Ba Tiêu Không Dạy, 125 
+81. Cao Đình Đánh Tăng, 127 
+82. Nham Đầu Cái Búa, 129 
+83. Ngưỡng Sơn Vẽ Một Đường, 131
+84. Càn Phong Một Đường, 133 
+85. Huyền Sa Thuyền Sắt, 134 
+86. Ngưỡng Sơn Ngồi Thiền, 136 
+87. Thiền Nguyệt Khảy Móng Tay, 137 
+88. Dược Sơn Cái Hồ, 122 
+89. Tuyết Phong Quả Cầu Gỗ, 141 
+90. Cái Mâm Vỡ, 142 
+91. Pháp Nhãn Giọt Nước, 143 
+92. Tào Sơn Bốn Không, 144 
+93. Đức Sơn Sư Tử, 146 
+94. Sống Một Mình, 148 
+95. Lâm Tế Mắt Chánh, 150 
+96. Nham Đầu Ba Giới, 152 
+97. Thân Phật, 154 
+98. Hưu Tịnh Dùng Trò Chơi, 156 
+99. Đại Điên Bao Nhiêu Tuổi, 159 
+100. Qui Sơn Phương Trượng, 163
 * */
 
 const Tiediji: Koan[] = [
