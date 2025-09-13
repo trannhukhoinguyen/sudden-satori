27d6b2e3f4f31202d7583c4dae18310f4c8427fc
Patriarchal Zen <trannhukhoinguyen@gmail.com>
2025-09-13 09:29:56 +0700

    Update YihuaShijie.ts

--- src/data/koans/YihuaShijie.ts
+++ src/data/koans/YihuaShijie.ts
@@ -5,7 +5,34 @@ THẾ GIỚI NHẤT HOA 365 CÔNG ÁN CỦA HT SÙNG SƠN
 * * */
 
 export const YihuaShijieTitles: KoanTitle[] = [
-
+  {
+   title: "Nhất niệm vạn niên khứ",
+   path: "/nhat-niem-van-nien-khu",
+  },
+  {
+   title: "Triêu Tịch Vấn Đạo",
+   path: "/trieu-tich-van-dao",
+  },
+  {
+   title: "Vô Ngại Vô Đắc",
+   path: "/vo-ngai-vo-dac",
+  },
+  {
+   title: "Chánh Pháp Nhãn tạng",
+   path: "/chanh-phap-nhan-tang",
+  },
+  {
+   title: "Thượng Thủ Kỷ Đa Phát",
+   path: "/thuong-thu-ky-da-thu",
+  },
+  {
+   title: "Tịch Tịch, Liêu Liêu",
+   path: "/tich-tich-lieu-lieu",
+  },
+  {
+   title: "Đô thị phóng hạ trước",
+   path: "/do-thi-phong-ha-truoc",
+  },
 ]
 
 const YihuaShijie: Koan[] = [
