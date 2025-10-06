6b0bb4cac723b5d2fe5865ff08c2baa57cacde48
trann <trannhukhoinguyen@gmail.com>
2025-10-06 14:31:39 +0700

    update content

---
+++ src/data/masters/MgMasterDb.tsx
@@ -0,0 +1,23 @@
+const MgMasterDb = [
+  {
+    id: '53.20.4',
+    name_en: 'Yelu Chucai',
+    name_ja: '',
+    name_zh: '',
+    name_vi: 'Gia Luật Sở Tài',
+    other_name_vi: '',
+    nickname_vi: '',
+    worship_tower: '',
+    birth_death_time: ['1190', '1244'],
+    records: '',
+    sect: ['Caodong'],
+    country: '🇲🇳',
+    place: '-',
+    teachers: ['Wansong Xingxiu'],
+    successors: [''],
+    disciples: [''],
+    image: '/images/avatar.png',
+    reference: ['https://thuvienhoasen.org/a39526/thung-dung-am-luc',],
+  },
+]
+export default MgMasterDb;
