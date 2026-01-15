f88dd0ed3be73cbf494c3dac989ae3d21aec6066
trann <trannhukhoinguyen@gmail.com>
2026-01-15 15:16:00 +0700

    update

--- src/utils/lunar.ts
+++ src/utils/lunar.ts
@@ -1,22 +1,17 @@
-// Có thể dùng thư viện nhỏ gọn
-// npm i lunar-javascript
 import { Solar } from 'lunar-javascript';
 
 export interface LunarDate {
     day: number;
     month: number;
     year: number;
-    isLeap: boolean;
 }
 
 export function getTodayLunar(): LunarDate {
-    const today = Solar.fromDate(new Date());
-    const lunar = today.getLunar();
+    const lunar = Solar.fromDate(new Date()).getLunar();
 
     return {
         day: lunar.getDay(),
         month: lunar.getMonth(),
         year: lunar.getYear(),
-        isLeap: lunar.isLeap(),
     };
 }
